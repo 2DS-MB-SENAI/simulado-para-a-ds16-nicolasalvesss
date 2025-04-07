2c15e27244a80799174d6facc061c279285dbe87
nicolasalvesss <nicolasvilelabarros@gmail.com>
2025-04-07 09:25:30 -0300

    ....

--- livros/urls.py
+++ livros/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.read, name='read'),
+    path('/livros/', views.read, name='read'),
     path('api/livros/', views.read_api),
 ]
