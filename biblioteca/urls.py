268214f058d76df53c4a478b5faa4e6e1d62e706
nicolasalvesss <nicolasvilelabarros@gmail.com>
2025-04-07 10:02:18 -0300

    ....

--- biblioteca/urls.py
+++ biblioteca/urls.py
@@ -19,6 +19,6 @@ from django.urls import path , include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('livros.urls')),
+    path('livros/', include('livros.urls')),
     path('api/', include('livros.urls'))
 ]
